a583e18381433ea73fd9f27a5ab25196d8257593
Boban Talevski <talevski.boban@gmail.com>
2017-12-24 02:13:58 +0100

    end of Binding Initiated video

--- app/src/main/java/com/bobantalevski/musicmachine/MainActivity.java
+++ app/src/main/java/com/bobantalevski/musicmachine/MainActivity.java
@@ -1,6 +1,10 @@
 package com.bobantalevski.musicmachine;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "song";
+    private boolean bound = false;
     private Button downloadButton;
     private Button playButton;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound) {
+            unbindService(serviceConnection);
+            // unbindService doesn't result in a call to onServiceDisconnected
+            // that's called only if something unexpected happens
+            // which is why we need to change the value of bound to false ourselves
+            bound = false;
+        }
     }
 }
